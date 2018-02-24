b4a2fd38695c0c15c4cca242e9fd4664cb014454
TMad <madsen.tyler@gmail.com>
2018-02-24 15:23:41 -0700

    add hour sentiment to sentiment contorller

--- app/controllers/api/v1/sentiment_controller.rb
+++ app/controllers/api/v1/sentiment_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::SentimentController < ApplicationController
   def show
-    render json: GoogleNaturalLanguageService.analyze(params[:text])
-
+    render json: HourSentiment.all
   end
 end
