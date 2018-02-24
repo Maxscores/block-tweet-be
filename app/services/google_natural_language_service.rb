b806b4e045531eb252ed6a783e73e51429474570
TMad <madsen.tyler@gmail.com>
2018-02-24 15:23:52 -0700

    change from entity sentiment to sentiment

--- app/services/google_natural_language_service.rb
+++ app/services/google_natural_language_service.rb
@@ -4,9 +4,9 @@ class GoogleNaturalLanguageService
   def self.analyze(text)
     language = Google::Cloud::Language.new
 
-    response = language.analyze_entity_sentiment(content: text, type: :PLAIN_TEXT)
+    response = language.analyze_sentiment(content: text, type: :PLAIN_TEXT)
 
-    response.entities
+    response.document_sentiment
   end
 
 end
