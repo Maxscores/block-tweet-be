cd18d0bab2a2d112c6c4bed2b59e5aae191a58b4
Max Stackhouse <maxstackhouse@gmail.com>
2018-02-25 09:25:40 -0700

    Hour sentiment endpoint (#4)
    * Add Serialization
    
    * BA-ify Serializer

--- app/serializers/group_hour_sentiment_serializer.rb
+++ app/serializers/group_hour_sentiment_serializer.rb
@@ -1,4 +1,8 @@
 class GroupHourSentimentSerializer < ActiveModel::Serializer
-  
   attributes :bitcoin, :ethereum, :blockchain, :crypto
+
+  has_many :bitcoin
+  has_many :ethereum
+  has_many :blockchain
+  has_many :crypto
 end
